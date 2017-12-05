234ec9753e22ce82d1b67a85dff0a5d2ea8f57b8
Piotr Kubica <piotr.kubica87@gmail.com>
2017-12-05 21:05:33 +0100

    3 more

--- python3/koans/triangle.py
+++ python3/koans/triangle.py
@@ -17,6 +17,9 @@
 #   about_triangle_project_2.py
 #
 def triangle(a, b, c):
+    if ((a <= 0 or b <= 0 or c <= 0) or (a + b <= c) or (a + c <= b) or (b + c <= 0)):
+        raise TriangleError
+    
     if (a == b and b == c):
         return 'equilateral'
     elif (a == b or b == c or a == c):
